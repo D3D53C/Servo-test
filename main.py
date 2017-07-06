d0ad0385babeb67ca3d11843f3a01413ac852b2f
D3D53C <marc.steinebrunner@gmail.com>
2017-07-06 15:40:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from servo import Servo as Servo
 import time
 import Rpi.GPIO as IO
 
-class main:
-    def __init__(self):
-        IO.setmode(BOARD)
-        IO.setwarnings(False)
-        IO.setup(35, IO.OUT)
-        IO.output(35, IO.HIGH)
-        self.servo = Servo(37,50,500,2500,180)
-        while True:
-            self.servo.change_position(0)
-            time.sleep(5)
-            self.servo.change_position(180)
-            time.sleep(5)
+def __init__(self):
+    IO.setmode(BOARD)
+    IO.setwarnings(False)
+    IO.setup(35, IO.OUT)
+    IO.output(35, IO.HIGH)
+    self.servo = Servo(37,50,500,2500,180)
+    while True:
+        self.servo.change_position(0)
+        time.sleep(5)
+        self.servo.change_position(180)
+        time.sleep(5)
